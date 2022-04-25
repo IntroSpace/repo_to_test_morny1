9ba38ceb9dc9652245c0cb32e442d2ecd3423256
IntroSpace <IntroSpace321@gmail.com>
2022-04-25 11:04:21 +0300

    beautiful print :)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print(f'you wrote: {input("enter something: ")}')
+class PCOLORS:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+print(f'{PCOLORS.BOLD}{PCOLORS.OKGREEN}That\'s {PCOLORS.OKCYAN}'
+      f'time{PCOLORS.OKGREEN} for a '
+      f'{PCOLORS.UNDERLINE}{PCOLORS.FAIL}11{PCOLORS.ENDC}'
+      f'{PCOLORS.BOLD}{PCOLORS.OKCYAN}:beautiful time:{PCOLORS.UNDERLINE}'
+      f'{PCOLORS.FAIL}11{PCOLORS.ENDC}{PCOLORS.BOLD}{PCOLORS.OKGREEN} '
+      f'he-he{PCOLORS.ENDC}')
